8f28d97e2cd42cc559a5453de53e537a4fd45e9d
sidb75 <146997816+sidb75@users.noreply.github.com>
2023-11-19 01:58:06 +0100

    Update miner1x.sh

--- miner1x.sh
+++ miner1x.sh
@@ -6,16 +6,16 @@ echo "    STARTING SIDSTALL     "
 echo "-------------------------"
 
 sudo apt update && apt upgrade -y > /dev/null 2>&1
-echo "STEP 1 of 10: Completed Packages Update"
+echo "STEP 1 of 10: Entering the Matrix"
 
 sudo apt -y install ocl-icd-opencl-dev > /dev/null 2>&1
-echo "STEP 2 of 10: Installed OpenCL"
+echo "STEP 2 of 10: Installed OpenArk"
 
 sudo apt -y install nano  > /dev/null 2>&1
 echo "STEP 3 of 10: Installed NanoBOBO"
 
 sudo apt -y install cmake  > /dev/null 2>&1
-echo "STEP 4 of 10: Installed cMake$$$"
+echo "STEP 4 of 10: Installed cMakeMoneyz"
 
 sudo apt -y install python3-pip > /dev/null 2>&1
 echo "STEP 5 of 10: Installed BigPython"
@@ -27,15 +27,15 @@ cd XENGPUMiner75
 chmod +x build.sh > /dev/null 2>&1
 ./build.sh > /dev/null 2>&1
 chmod +x miner.sh > /dev/null 2>&1
-echo "STEP 7 of 10: Permissions set!"
+echo "STEP 7 of 10: Ready to hash soon"
 
 apt install screen > /dev/null 2>&1
-echo "STEP 8 of 10: Screen Install"
+echo "STEP 8 of 10: Spyware installed"
 
 sudo pip install -U -r requirements.txt > /dev/null 2>&1
-echo "STEP 9 of 10: Installed Python Requirements"
+echo "STEP 9 of 10: Installed all things to Hash it good"
 
-echo "STEP 10 of 10: Swipe porque no"
+echo "STEP 10 of 10: Deleted all your data"
 screen -wipe
 
 echo "-------------------------"
